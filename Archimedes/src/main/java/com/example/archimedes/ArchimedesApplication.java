156abc375d7f1acf5005cb6c600f1c356c6cbb30
Yael Greenberg <117977974+MyNameIsYg@users.noreply.github.com>
2024-02-05 11:43:57 +0200

    Add files via upload

---
+++ Archimedes/src/main/java/com/example/archimedes/ArchimedesApplication.java
@@ -0,0 +1,16 @@
+package com.example.archimedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ArchimedesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchimedesApplication.class, args);
+    }
+
+}
